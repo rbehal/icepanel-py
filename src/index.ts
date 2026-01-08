cf754483a72af361a4e3e5d55615afd9309b4a4e
Victor Leach <victor@icepanel.io>
2026-01-08 13:41:00 -0800

    Add helper classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-export * as IcePanel from "./api/index.js";
 export type { BaseClientOptions, BaseRequestOptions } from "./BaseClient.js";
-export { IcePanelClient } from "./Client.js";
+export { IcePanelClient } from "./IcePanelClient.js";
 export { IcePanelEnvironment } from "./environments.js";
 export { IcePanelError, IcePanelTimeoutError } from "./errors/index.js";
 export * from "./exports.js";
+export * from './types.js';
+export * as IcePanel from './types.js';
+export * from './helpers/index.js';
+export * from './consts.js';
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export * from "./api/requests/index.js";
+export * from "./api/types/index.js";
+export * from "./consts.js";
